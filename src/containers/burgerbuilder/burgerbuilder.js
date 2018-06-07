7ea751bbe8ff0171c3edcab050993827e972070a
Stanley Huang <infamous_godhand@yahoo.com>
2018-06-07 12:20:34 +0000

    passing ingredients via query to the checkout page

--- src/containers/burgerbuilder/burgerbuilder.js
+++ src/containers/burgerbuilder/burgerbuilder.js
@@ -125,7 +125,17 @@ class BurgerBuilder extends Component {
                 this.setState({ loading: false, purchasing: false })
             });
       */
-        this.props.history.push("/checkout");
+        const queryParams = [];
+        for (var i in this.state.ingredients) {
+            queryParams.push(encodeURIComponent(i) + "=" + encodeURIComponent(this.state.ingredients[i]));
+        }
+        const queryString = queryParams.join("&");
+
+        this.props.history.push({
+            pathname: "/checkout",
+            search: "?" + queryString
+
+        });
     }
 
 
