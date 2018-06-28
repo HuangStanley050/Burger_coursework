82ecb5593c7733a8da6f5f73e04bd202ca145c22
Stanley Huang <infamous_godhand@yahoo.com>
2018-06-28 13:18:18 +0000

    created auth action creators

--- src/containers/auth/auth.js
+++ src/containers/auth/auth.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import * as actions from "../../store/actions/index";
 import Input from "../../components/ui/input/input";
 import Button from "../../components/ui/button/button";
 import "./auth.css";
@@ -71,6 +73,11 @@ class Auth extends Component {
         this.setState({ controls: updatedControls });
     }
 
+    submitHandler = (event) => {
+        event.preventDefault();
+        this.props.onAuth(this.state.controls.email.value, this.state.controls.password.value);
+    }
+
     render() {
         const formElementsArray = [];
 
@@ -99,7 +106,7 @@ class Auth extends Component {
 
         return (
             <div className="Auth">
-                <form>
+                <form onSubmit={this.submitHandler}>
                   {form}
                   <Button btnType = "Success">SUBMIT</Button>
                 </form>
@@ -108,4 +115,10 @@ class Auth extends Component {
     }
 }
 
-export default Auth;
+const mapDispatchToProps = dispatch => {
+    return {
+        onAuth: (email, password) => dispatch(actions.auth(email, password))
+    };
+};
+
+export default connect(null, mapDispatchToProps)(Auth);
--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -1,2 +1,3 @@
 export { addIngredient, removeIngredient, initIngredients } from "./burgerBuilder.js";
 export { purchaseBurger, purchaseBurgerFail, purchaseBurgerSuccess, purchaseInit, fetchOrders } from "./order.js";
+export { auth } from "./auth";
