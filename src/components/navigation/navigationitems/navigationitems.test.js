de3d06ed5178fe95adeca38e7b49667d9d2b3dbf
Stanley Huang <infamous_godhand@yahoo.com>
2018-07-11 12:44:31 +0000

    added more test for container and redux

--- src/components/navigation/navigationitems/navigationitems.test.js
+++ src/components/navigation/navigationitems/navigationitems.test.js
@@ -8,8 +8,25 @@ import NavigationItem from "./navigationitem/navigationitem";
 configure({ adapter: new Adapter() });
 
 describe("<NavigationItems/>", () => {
+    let wrapper =
+
+        beforeEach(() => {
+            wrapper = shallow(<NavigationItems/>);
+        });
+
     it("should render two <NavigationItem/> elements if not authenticated", () => {
-        const wrapper = shallow(<NavigationItems/>);
+
         expect(wrapper.find(NavigationItem)).toHaveLength(2);
     });
-})
+
+    it("should render three <NavigationItem/> elements if authenticated", () => {
+        //wrapper = shallow(<NavigationItems isAuthenticated />);
+        wrapper.setProps({ isAuthenticated: true });
+        expect(wrapper.find(NavigationItem)).toHaveLength(3);
+    });
+
+    it("should render exact logout button", () => {
+        wrapper.setProps({ isAuthenticated: true });
+        expect(wrapper.contains(<NavigationItem link="/logout">Logout</NavigationItem>)).toEqual(true);
+    });
+});
